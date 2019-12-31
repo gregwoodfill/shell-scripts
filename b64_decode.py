dac55d33449e69032c1e9aab04f90639747f47ed
Greg Woodfill <greg@gregwoodfill.com>
2019-12-30 21:13:09 -0600

    add docstrings

--- b64_decode.py
+++ b64_decode.py
@@ -3,5 +3,10 @@ import base64
 
 
 def decodeBase64(text: str):
+    '''decode utf-8 input to base 64'''
     txt_bytes = text.encode('utf-8')
     return base64.standard_b64decode(txt_bytes).decode('utf-8')
+
+
+if __name__ == "__main__":
+    print(decodeBase64(sys.argv[1]))
